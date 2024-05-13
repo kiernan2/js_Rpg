5a5011069ea345cef203def48fc01ae109f97da4
Kiernan2 <kiernan1994@gmail.com>
2024-05-13 15:56:10 -0700

    Rough start mostly an outline

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let charaList = [] // Global variable remove later
+
+const generateCharacter = () => {
+  let chara = {};
+  if (characterType === "Player")
+    {}
+  else if (characterType === "Monster")
+    {}
+  else if (characterType === "Ally")
+    {}
+}
+
+const updateStat = (chara, stat, value) => {
+  return ();
+}
